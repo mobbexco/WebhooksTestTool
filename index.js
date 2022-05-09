e8ad4e3b1818b102861aa09fe205c46990868a89
bcastellina <bruno-castellina_23@hotmail.com>
2022-05-09 13:38:53 -0300

    Update Tool

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 
-const request = require('request'),
-    data = require('./data.json');
+const axios = require('axios'),
+    data = require('./data.json'),
+    qs = require('qs');
 
 const LOG_TAG = 'Webhooks Test Tool > ';
 
@@ -9,22 +10,48 @@ const debug = (...msg) => {
     console.info(LOG_TAG, ...msg);
 };
 
-const executeWebHook = async ({ url, form }) => {
+const executeWebHook = async ({ url, data }) => {
     var payload = {
-        url,
+        url: url ?? process.env.WEBHOOK_URL,
         method: 'POST',
-        form,
         timeout: 30000
     };
+
+    const format = process.env.FORMAT ?? 'json';
+
+    if (format === 'json') {
+        payload.data = data;
+    } else if (format === 'form') {
+        payload.data = qs.stringify(data);
+        payload.headers = {
+            'content-type': 'application/x-www-form-urlencoded'
+        }
+    } else {
+        debug("FORMAT NOT SUPPORTED");
+        return;
+    }
 
     // Log the call
     debug(payload);
 
-    request(payload, (err, response, body) => {
-        debug(err);
+    try {
+        const response = await axios(payload);
+
+        debug("--- Webhook Sucessfully Send! ---");
         debug(response.headers);
-        debug(body);
-    });
+        debug(response.data);
+    } catch(error) {
+        if (error.response) {
+            debug(error.response.headers);
+            debug(error.response.data);
+
+        } else if(error.request) {
+            debug("--- No response from destination server ---");
+
+        } else {
+            debug(error.message);
+        }
+    }
 };
 
 const bootstrap = async () => {
--- readme.md
+++ readme.md
@@ -11,6 +11,7 @@ Herramienta para probar WebHooks localmente simulando el funcionamiento de un We
 - Correr ```npm install``` para instalar dependencias
 
 ### Funcionamiento
-- Cambiar la URL del WebHook desde .env
+- Defina una URL global para el envío de notificaciones desde la variable *WEBHOOK_URL* en el archivo **.env** o en cada uno de los webhooks desde el archivo **data.json**
+- Defina el formato de envío del webhook *"json"* o *"form"*. Por defecto se envía en formato **json**
 - Cambiar si es necesario el JSON dentro de data.json
 - Correr con ```npm start```
